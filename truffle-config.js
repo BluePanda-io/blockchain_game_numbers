c211bd815aaffa14a641dab5a286f8c5fcb4bf10
milts10 <miltossaratzidis1@gmail.com>
2021-12-30 17:18:06 +0100

    gsd

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+// const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
   networks: {
@@ -9,6 +14,19 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          // Private Key
+          privateKeys.split(','),
+          // Url to an Ethereum Node
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+        );
+      },
+      gass: 5000000,
+      gasPrice:25000000000,
+      network_id: 42,
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
