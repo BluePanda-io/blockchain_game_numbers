967cabeaf54daa80311cb4427296c4789c479b76
milts10 <miltossaratzidis1@gmail.com>
2021-12-31 19:29:30 +0100

    f

--- src/redux/actions/web3Actions.js
+++ src/redux/actions/web3Actions.js
@@ -30,8 +30,9 @@ export const web3Initialize = () => {
         let web3Info = {}
 
         // try {
-            console.log("tt -> 1",provider)
-            window.web3 = new Web3(provider)
+            console.log("tt -> 1",ethereum)
+            console.log("tt -> 1.5",provider)
+            window.web3 = new Web3(ethereum)
 
 
             web3Info["window_web3"] = window.web3 
