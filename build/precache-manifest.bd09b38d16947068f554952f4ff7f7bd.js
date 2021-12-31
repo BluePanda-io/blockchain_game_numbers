28c39e8ed3b0c72060914f7c63e2a35b44db4738
milts10 <miltossaratzidis1@gmail.com>
2021-12-31 18:30:09 +0100

    f

---
+++ build/precache-manifest.bd09b38d16947068f554952f4ff7f7bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2056bc386ad4d0ce44bcd8aa7b0573",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b9c815d68a02fb369d3",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9eb459afab5aeedbdb0d",
+    "url": "./static/js/2.5ac2899b.chunk.js"
+  },
+  {
+    "revision": "0f07b78795014ad157cf3cc8520f0393",
+    "url": "./static/js/2.5ac2899b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01d1566837bf093ce97",
+    "url": "./static/js/3.15aff5b9.chunk.js"
+  },
+  {
+    "revision": "1b9c815d68a02fb369d3",
+    "url": "./static/js/main.06fcee14.chunk.js"
+  },
+  {
+    "revision": "8e41a5841df9d6880229",
+    "url": "./static/js/runtime-main.d20fbb32.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
